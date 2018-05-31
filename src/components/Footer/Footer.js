41dc9575cd672ea25c89e0a607a01bb06cb169a6
DanBaxt <danzbaxt@gmail.com>
2018-05-30 18:12:57 -0700

    Starting work on project
    Begun work on basic layout

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <span>Sonata Arctica Fanpage</span>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+const Header = (props) => (
+  <footer className="header">
+    <span>Sonata Arctica Fanpage!</span>
+  </footer>
+);
+
+export default Header;
