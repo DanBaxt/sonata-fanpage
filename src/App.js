00019fa285bdfd7e25855a059df03ef7f85d3c86
DanBaxt <danzbaxt@gmail.com>
2018-09-12 17:58:55 -0700

    Added new content
    Added second carousel. Not yet fully implemented

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import CarouselA from "./components/Carousel";
 import Header from "./components/Header";
 import Jumbo from "./components/Jumbotron";
+import CarouselPage from "./components/Carousel2";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import logo from './logo.svg';
 import './App.css';
+
+
 
 const App = () => (
   <Router>
@@ -19,6 +22,7 @@ const App = () => (
       <Wrapper>
 
       </Wrapper>
+      <CarouselPage />
       <Footer />
     </div>
   </Router>
